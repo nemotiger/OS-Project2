6b64fbe1db1f7bbc25eaca083936c396007f128c
Nemotiger <nemotiger100@gmail.com>
2018-06-19 22:08:00 +0800

    fix wrong file size output

--- src/user_program/master.c
+++ src/user_program/master.c
@@ -97,7 +97,7 @@ int main (int argc, char* argv[])
 	}
 	gettimeofday(&end, NULL);
 	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
-	printf("Transmission time: %lf ms, File size: %lu bytes\n", trans_time, file_size / 8);
+	printf("Transmission time: %lf ms, File size: %lu bytes\n", trans_time, file_size);
 
 	close(file_fd);
 	close(dev_fd);
